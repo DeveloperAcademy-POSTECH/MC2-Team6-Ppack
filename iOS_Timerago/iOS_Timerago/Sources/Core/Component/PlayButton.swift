c4188c155a04156c7f4d7eaf0344d5ba1a05dd91
Hamp <yongbeomkwak@gmail.com>
2023-05-01 19:58:43 +0900

    :guitar: :: Color with hexcode 생성자 추가

---
+++ iOS_Timerago/iOS_Timerago/Sources/Core/Component/PlayButton.swift
@@ -0,0 +1,20 @@
+//
+//  PlayButton.swift
+//  iOS_Timerago
+//
+//  Created by yongbeomkwak on 2023/05/01.
+//
+
+import SwiftUI
+
+struct PlayButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlayButton_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayButton()
+    }
+}
