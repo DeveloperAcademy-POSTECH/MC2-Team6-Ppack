8445ff116d1d9fffc55bd213cdf48330821c6e20
Hamp <yongbeomkwak@gmail.com>
2023-05-11 12:12:40 +0900

    :lipstick: :: 타이머 뷰 스켈레톤 완성

--- iOS_Timerago/iOS_Timerago/Sources/Core/Timer/TimerView.swift
+++ iOS_Timerago/iOS_Timerago/Sources/Core/Timer/TimerView.swift
@@ -11,19 +11,78 @@ struct TimerView: View {
     
     @Binding var routine:RoutineModel
     @State var time:String = ""
+    @State var width:CGFloat = 0
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
         
-        VStack(spacing:0){
+        GeometryReader{ geometry in
             
-            topArea
-            bottomArea
+            ZStack{
+                topArea
+                    .frame(height:geometry.size.height/2)
+                    .position(x: geometry.size.width / 2,y: geometry.size.height/4)
+                
+                VStack(spacing:10){
+                    
+                    HStack(spacing:0){
+                        ForEach(routine.task){ task in
+                            
+                            Text("\(task.emoji)")
+                                .font(.system(size: 24))
+                                .frame(maxWidth: .infinity)
+                            
+                        }
+                    }
+                    .frame(width:.infinity,height:30,alignment: .leading)
+                    
+                    RoundedRectangle(cornerRadius:12)
+                        .fill(Color.white)
+                        .frame(width:.infinity,height:20)
+                        .padding(.horizontal,20)
+                        .overlay(alignment:.leading){
+                            RoundedRectangle(cornerRadius:12)
+                                .fill(Color.accent)
+                                .frame(width:width)
+                                .padding(.horizontal,20)
+                        }
+                        .shadow(color: .black.opacity(0.12), radius: 5, x: 0, y: 3)
+
+                    
+                    
+                    
+                    
+                    
+                }
+                .position(x:geometry.size.width / 2,y:geometry.size.height/2 - 20)
+                .frame(width:geometry.size.width)
+                
+                .zIndex(2.0)
+                
+
+                bottomArea
+                    .frame(height:geometry.size.height/2)
+                    .position(x: geometry.size.width / 2,y:geometry.size.height / 4 + geometry.size.height / 2 )
+            }
+                
+                
+               
+
+
+            
             
             
         }
         .onAppear{
             time = String(routine.task.map{Int($0.interval) ?? 0}.reduce(0, {$0 + $1}))
         }
+        
+        .onReceive(timer) { _ in
+            withAnimation(.easeInOut){
+                width += 1
+            }
+        }
+        
         
         
     }
@@ -38,7 +97,7 @@ struct TimerView: View {
                 
             
             
-        }.frame(height: UIScreen.height/2)
+        }
     }
     
     private var bottomArea: some View {
@@ -64,13 +123,13 @@ struct TimerView: View {
             }.padding(.bottom,UIScreen.height/6)
 
             
-        }.frame(height: UIScreen.height/2)
+        }
     }
 }
 
 struct TimerView_Previews: PreviewProvider {
     static var previews: some View {
-        TimerView(routine: .constant(RoutineModel(task: [TaskModel(emoji: "✅", interval: "5"),TaskModel(emoji: "✅", interval: "10"),TaskModel(emoji: "✅", interval: "5"),TaskModel(emoji: "✅", interval: "20")], title: "Hello")))
+        TimerView(routine: .constant(RoutineModel(task: [TaskModel(emoji: "✅", interval: "5"),TaskModel(emoji: "✅", interval: "10"),TaskModel(emoji: "✅", interval: "5"),TaskModel(emoji: "✅", interval: "20"),TaskModel(emoji: "✅", interval: "20"),TaskModel(emoji: "✅", interval: "20")], title: "Hello")))
             .previewLayout(.sizeThatFits)
     }
 }
