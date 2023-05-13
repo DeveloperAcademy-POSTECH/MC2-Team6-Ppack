142489b2e0ce10988afb2658f1a2cdf46e6c8966
Kiwi <toskw@naver.com>
2023-05-13 23:19:51 +0900

    :bug: :: 타이머뷰 중복 코드 제거

--- iOS_Timerago/iOS_Timerago/Sources/Core/Timer/TimerView.swift
+++ iOS_Timerago/iOS_Timerago/Sources/Core/Timer/TimerView.swift
@@ -132,14 +132,6 @@ struct TimerView: View {
             }
             viewModel.isActive = true
         }
-        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
-            if let backgroundTime = viewModel.backgroundTime {
-                let timeInterval = Date().timeIntervalSince(backgroundTime)
-                viewModel.timeInterval = Int(timeInterval)
-                
-            }
-            viewModel.isActive = true
-        }
         .onReceive(NotificationCenter.default.publisher(for: UIScene.didDisconnectNotification)) { _ in
             viewModel.tapStopButton()
         }
